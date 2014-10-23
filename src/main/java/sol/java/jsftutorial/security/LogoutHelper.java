4ddf2897aea70a51dd2acae5d7be49437ad627f6
Christian Peter <peter.chr@web.de>
2014-10-23 10:39:05 +0200

    logout button added

---
+++ src/main/java/sol/java/jsftutorial/security/LogoutHelper.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ *//*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sol.java.jsftutorial.security;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author debiandev
+ */
+@Named
+@RequestScoped
+public class LogoutHelper implements Serializable{
+
+    public String logout(){
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        System.out.println("vix ext ctx " + ctx.getExternalContext().getUserPrincipal());
+        final ExternalContext extCtx = ctx.getExternalContext();
+        final HttpSession session = (HttpSession) extCtx.getSession(false);
+        if (session != null){
+            session.invalidate();
+            try {
+                extCtx.redirect("login.html");
+            } catch (IOException ex) {
+                Logger.getLogger(LogoutHelper.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        
+        return "";
+    }
+}
